29bd284444674fce352ad99fc0636cb5fc666c2c
Thrishank Kalluru <thrishankkalluru@gmail.com>
2022-08-27 16:12:44 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React,{ Component } from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
+import Particles from 'react-particles-js'; 
 
+const ParticlesOptions = {
+  Particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA901",
+        blur: 5
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
   return (
     <div className="App">
+    <Particles
+      params={ParticlesOptions}
+    />
     <Navigation />
     <Logo />
-    {/* <ImageLinkForm />
-    <FaceRecognition /> */} 
+    <Rank />
+    <ImageLinkForm />
+    {/* <FaceRecognition />  */}
     </div>
   );
 }
